2f6b9954e68dd8859f755ca8499715883ae5bb82
LubiePierogi <26669430+LubiePierogi@users.noreply.github.com>
2019-07-19 20:34:49 +0200

    camera moves

---
+++ BF Gierka/Assets/FajnaCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FajnaCamera : MonoBehaviour
+{
+    public Rigidbody2D player;
+
+    public float velocityWeight = 2.0f;
+    public float velocitySpeed = 0.02f;
+    public float followSpeed = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (player != null)
+        {
+            Vector2 oldCam = transform.localPosition;
+            Vector2 playerVelocity = player.GetComponent<Rigidbody2D>().velocity;
+            float v = playerVelocity.magnitude;
+            float mult = velocitySpeed * v;
+            mult = mult / (mult * mult + 1.0f);
+            Vector2 destCam = (Vector2)player.transform.localPosition + velocityWeight * mult * playerVelocity.normalized;
+            float followAmount = followSpeed * Time.deltaTime;
+            Vector2 finalCam = followSpeed * destCam + (1.0f - followSpeed) * oldCam;
+            transform.localPosition = new Vector3(finalCam.x, finalCam.y, transform.localPosition.z);
+        }
+
+    }
+}
