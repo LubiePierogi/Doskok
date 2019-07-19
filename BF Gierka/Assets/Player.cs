5a6e354d0abb674e7e6aefdf0a63d3ee698d6c60
unknown <nudy148@gmail.com>
2019-07-19 19:21:54 +0200

    wall jump

--- BF Gierka/Assets/Player.cs
+++ BF Gierka/Assets/Player.cs
@@ -5,31 +5,27 @@ using UnityEngine.EventSystems;
 public class Player : MonoBehaviour
 {
     public float V = 0.1f;
+    Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hitInfo = new RaycastHit();
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
-        RaycastHit2D hit = Physics2D.Raycast(Camera.main.transform.position, Vector2.zero);
-        if (hit)
-        {
-            
-                GameObject target = hit.transform.gameObject;
-            Debug.Log(target);
-            PlatformScript tscript = target.GetComponent<PlatformScript>();
-                if (tscript)
-            {
-                //tscript.Grow(V);
+        
+       
+    }
 
-            }
-           
-        }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        rigidbody.velocity = Vector2.zero;
+        rigidbody.gravityScale = 0;
+    }
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        rigidbody.gravityScale = 1;
     }
 }
