154372b574f63a38d34e9f15bdec9f93b0e4f96f
Gobral <umpalump97@gmail.com>
2019-07-20 10:00:24 +0200

    usunięto odklejanie przy kilku stykach z platformami, dodano poruszanie z prawym myszkiem

--- BF Gierka/Assets/Player.cs
+++ BF Gierka/Assets/Player.cs
@@ -7,7 +7,11 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     public float V = 0.1f;
+    public int styki = 0;
     Rigidbody2D rigidbody;
+    public float Power = 1.0f;
+    public float amount = 3.0f;
+    public List<int> kolizje;
 
     public void ReadResetKey()
     {
@@ -22,6 +26,7 @@ public class Player : MonoBehaviour
     private bool sitting = false;
     private Collider2D myColl;
     public Vector3 futureDir;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -41,17 +46,32 @@ public class Player : MonoBehaviour
             //Physics2D.IgnoreCollision(path.GetComponent<Collider2D>(), myColl);
             //path.GetComponent<Rigidbody2D>().AddForce(futureDir*10, ForceMode2D.Impulse);
         }
+        if (styki >= 1 && Input.GetMouseButtonDown(1))
+        {
+            rigidbody.velocity = Vector2.zero;
+        }
+        if (styki >= 1 && Input.GetMouseButton(1))
+        {
+            // int ruchH = Input.GetAxisRaw("Horizontal");
+            // int ruchV = Input.GetAxisRaw("Vertical");
+            Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+            dir = dir * amount * Time.deltaTime;
+            rigidbody.AddForce((dir) * Power, ForceMode2D.Impulse);
+        }
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
         rigidbody.velocity = Vector2.zero;
         rigidbody.gravityScale = 0;
-        sitting = true;
+        styki++;
+        kolizje.Add(collision.gameObject.GetInstanceID());
     }
     public void OnCollisionExit2D(Collision2D collision)
     {
-        rigidbody.gravityScale = 1;
-        sitting = false;
+        --styki;
+        kolizje.Remove(collision.gameObject.GetInstanceID());
+        if (styki == 0)
+            rigidbody.gravityScale = 1;
     }
 }
