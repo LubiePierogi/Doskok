64b4c0a0d040cd31db54bf290607d31e5b36b485
Krzysztof Rymski <nudy148@gmail.com>
2019-07-20 13:30:10 +0200

    Textures

---
+++ BF Gierka/Assets/leafesScaler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leafesScaler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform parent;
+    private SpriteRenderer sprite;
+    void Start()
+    {
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(1 / parent.localScale.x, 1 / parent.localScale.y, 1 / parent.localScale.z);
+        sprite.size = parent.localScale;
+    }
+}
