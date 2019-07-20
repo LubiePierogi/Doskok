146f5ef8a03dafd4261b7a79ec3caa82eb865863
LubiePierogi <26669430+LubiePierogi@users.noreply.github.com>
2019-07-20 13:41:17 +0200

    menu

---
+++ BF Gierka/Assets/Menu/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "MainMenu", menuName = "bfg/mainmenu", order = 1)]
+public class MainMenu : ScriptableObject
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("scene2");
+    }
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
